b6a8e9f399c52863319b45bd019ac5d9c10aedcb
rakesh050791 <rakesh.joshi@mobiloitte.com>
2014-01-14 09:15:11 +0530

    updated services without bug

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,10 +5,9 @@ class HomesController < ApplicationController
 		@terms = Terms.all
 		unless @terms.empty?
 		  render :json => {
-		  	      :responseCode => "200",
-		  	      :responseMessage => @terms.first.description 
-		                  }
-	                     
+						:responseCode => "200",
+						:responseMessage => @terms.first.description
+	 						}  	                     
 	     else
 	      render :json => {
 	       		    :responseCode => "500",
@@ -21,9 +20,9 @@ class HomesController < ApplicationController
 		@privacy = Privacy.all
 		unless @privacy.empty?
 		  render :json => {
-				  	:responseCode => "200",
-				  	:responseMessage  => @privacy.first.description
-		  	              }
+						:responseCode => "200",
+						:responseMessage => @privacy.first.description
+	 						}  	
 	     else
 	      render :json => {
 	       		    :responseCode => "500",
@@ -35,52 +34,54 @@ class HomesController < ApplicationController
 	def category
 		@categories = Category.all
 		unless @categories.empty?
-			a = []
-			@categories.each do |cat|
-			 		a << cat.title
-			end
+			 @categories.each do |cat|
+			   cat[:id] = cat.id
+			   cat[:title] = cat.title
+			   cat[:description] = cat.description
+		      end
 				 render :json => {
-                    :responseCode => "200",
-				 	:responseMessage => a 
-				                 }
-			else
+						:responseCode => "200",
+						:responseMessage => @categories.as_json(:only =>[:id,:title,:description])	
+	 						     }
+		else
 			    render :json => {
 						:responseCode => "500",
 						:responseMessage => "No such result found"	
 	 						     }
-	 	    end
+	 	end
 	end
 
 	def sub_category
 		@sub_categories = SubCategory.find_all_by_category_id(params[:id])
 		unless @sub_categories.empty?
-			b = []
 			@sub_categories.each do |cat|
-			 		b << cat.title
+			   cat[:id] = cat.id
+			   cat[:title] = cat.title
+			   cat[:description] = cat.description
 			end
 				 render :json => {
-				 	:responseCode => "200",
-				 	:responseMessage =>  b.as_json
-				 	             }
-			else
+						:responseCode => "200",
+						:responseMessage => @sub_categories.as_json(:only =>[:id,:title,:description])	
+	 						     }
+		else
 			    render :json => {
 						:responseCode => "500",
 						:responseMessage => "No such result found"	
 	 						     }
-	 	    end
+	 	end
 	end
 
 	def item
 		@items = Item.find_all_by_sub_category_id(params[:id])
 		unless @items.empty?
-			c = []
 			@items.each do |item|
-			c << item.title
+		    item[:id] = item.id		
+			item[:title] = item.title
 			end
-			    render :json => {
-			    			:responseCode => "200",
-						    :responseMessage => c.as_json 
-			    				 }
+			   render :json => {
+						:responseCode => "200",
+						:responseMessage => @items.as_json(:only =>[:id,:title])	
+	 						    }
 		else
 			    render :json => {
 						:responseCode => "500",
@@ -93,14 +94,15 @@ class HomesController < ApplicationController
 		@items = Item.find_all_by_id(params[:id])
 		unless @items.empty?
 			 @items.each do |item|
+			     item[:id] = item.id				 	 
 			     item[:title] = item.title
 			     item[:description] = item.description
 			     item[:type] = item.item_type
 		     end
 		         render :json => {
-		         			:responseCode => "200",
-						    :responseMessage => @items.as_json(:only =>[:title,:description,:type])
-		         				}
+							:responseCode => "200",
+							:responseMessage => @items.as_json(:only =>[:id,:title,:description,:type])
+	    						 }
 	    else
 			     render :json => {
 							:responseCode => "500",
